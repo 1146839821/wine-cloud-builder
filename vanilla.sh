9dba6796c938c901421fa724adbda2d9354a8915
zhang bo <bobo1146839821@qq.com>
2026-04-30 21:06:14 +0800

    Update vanilla.sh

--- vanilla.sh
+++ vanilla.sh
@@ -143,7 +143,7 @@ ${WINE_CONFIGURE} \
     --without-opencl \
     --disable-amd_ags \
     --disable-amd_ags_x64 \
-    --without-opengl
+    --without-opengl \
     --without-oss \
     --with-pcap \
     --with-pthread \
